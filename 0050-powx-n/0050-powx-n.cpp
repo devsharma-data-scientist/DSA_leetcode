67207f8b19a821809b1d4553b6601e63454471d5
devsharma-data-scientist <devbhraman5@gmail.com>
2026-06-29 12:05:30 +0530

    Time: 0 ms (100%), Space: 8.7 MB (11.95%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        long long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        if (N == 0)
+            return 1;
+
+        if (N % 2 == 0)
+            return myPow(x * x, N / 2);
+
+        return x * myPow(x * x, N / 2);
+    }
+};
