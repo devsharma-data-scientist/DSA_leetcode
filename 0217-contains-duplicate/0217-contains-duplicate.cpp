3a40534d71c4d19c2d033444d8644b9d9fa3041f
Dev Sharma <devbhraman5@gmail.com>
2026-07-12 20:19:33 +0530

    Time: 158 ms (5.11%), Space: 113.7 MB (5.24%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int, int> mpp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (mpp[nums[i]] >= 1)
+                return true;
+
+            mpp[nums[i]]++;
+        }
+
+        return false;
+    }
+};
