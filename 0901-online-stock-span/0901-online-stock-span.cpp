9424e4e1c5f6d944f20c7ad13bf6adfac4e1d683
Dev Sharma <devbhraman5@gmail.com>
2026-07-22 05:38:10 +0530

    Time: 35 ms (43.96%), Space: 94.1 MB (8.63%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+class StockSpanner {
+public:
+    vector<int> prices;
+    stack<int> st;   // stores indices
+
+    StockSpanner() {
+
+    }
+
+    int next(int price) {
+        prices.push_back(price);
+        int i = prices.size() - 1;
+
+        while (!st.empty() && prices[st.top()] <= price) {
+            st.pop();
+        }
+
+        int span;
+        if (st.empty())
+            span = i + 1;
+        else
+            span = i - st.top();
+
+        st.push(i);
+
+        return span;
+    }
+};
