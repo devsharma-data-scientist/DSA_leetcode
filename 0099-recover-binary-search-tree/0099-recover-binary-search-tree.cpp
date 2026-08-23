9b41f941224eb8fc87af243066c03ee3a6932277
Dev Sharma <devbhraman5@gmail.com>
2026-08-23 11:27:12 +0530

    Time: 0 ms (100%), Space: 61.3 MB (10.25%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,24 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<TreeNode*> nodes;
-    vector<int> values;
+    vector<int> v;
 
-    void inorder(TreeNode* root) {
+    void inorder(TreeNode* root){
         if(root == NULL) return;
 
         inorder(root->left);
-        nodes.push_back(root);
-        values.push_back(root->val);
+        v.push_back(root->val);
         inorder(root->right);
+        return;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+    return solve(nums, 0, nums.size() - 1);
+}
+
+    TreeNode* solve(vector<int>& nums, int low, int high) {
+        if (low > high)
+            return NULL;
+
+        int mid = low + (high - low) / 2;
+
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = solve(nums, low, mid - 1);
+        root->right = solve(nums, mid + 1, high);
+
+        return root;
     }
 
     void recoverTree(TreeNode* root) {
-        inorder(root);
+    inorder(root);
+    sort(v.begin(), v.end());
 
-        sort(values.begin(), values.end());
+    int i = 0;
 
-        for(int i = 0; i < nodes.size(); i++) {
-            nodes[i]->val = values[i];
-        }
-    }
+    function<void(TreeNode*)> change = [&](TreeNode* root) {
+        if (root == NULL) return;
+
+        change(root->left);
+        root->val = v[i++];
+        change(root->right);
+    };
+
+    change(root);
+}
 };
